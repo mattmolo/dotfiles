0a5cd222fb30bcee0eae6eeb8ba02c31e5e24628
Matt Molo <molo.matt@gmail.com>
2014-05-20 16:06:23 -0500

    edited aliases file

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 ########## Variables
 dir=~/.dotfiles                   # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim"          # list of files/folders to symlink in homedir
+files="bashrc aliases vimrc vim"          # list of files/folders to symlink in homedir
 ##########
 
 # create dotfiles_old in homedir
