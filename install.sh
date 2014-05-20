339d63911addd5bddffa3a36f2be4c947ef3969e
Matt Molo <molo.matt@gmail.com>
2014-05-20 15:49:11 -0500

    more installer things

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-!$/bin/bash
+#!/bin/bash
 ############################
 # This script creates symlinks from the home directory to any desired dotfiles in ~/.dotfiles
 ############################
@@ -10,20 +10,29 @@ files="bashrc vimrc vim"          # list of files/folders to symlink in homedir
 ##########
 
 # create dotfiles_old in homedir
-echo -e "[31mCreating $olddir for backup of any existing dotfiles in ~"
-rm -r $olddir
+echo -e "\e[46mCreating $olddir for backup of any existing dotfiles in ~\e[0m"
+
+if [ ! -d $olddir/ ]; then
+    read -p "\e[31m$olddir already exists. Delete it?\e[0m" -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]] then
+        echo -e "\e[31mRemoving $olddir\e[0m"
+        rm -r $olddir
+    fi    
+fi
+
 mkdir -p $olddir
 
-# change to the dotfiles directory
-echo -e "[31mChanging to the $dir directory"
-cd $dir
-
 # move any existing dotfiles in homedir to old dotfiles directory, then create symlinks from the homedir to any files in the dotfiles directory specified in $files
-echo -e "[31mMoving any existing dotfiles from ~ to $olddir"
+cd $dir
+echo -e "\e[46mMoving any existing dotfiles from ~ to $olddir\e[0m"
 for file in $files; do
     mv ~/.$file $olddir
-    echo -e "[31mCreating symlink to $file in home directory."
+    echo -e "\e[46mCreating symlink to $file in home directory\e[0m"
     ln -s $dir/$file ~/.$file
 done
 
-sh install_omz.sh
+# ask to install oh-my-zsh
+read -p "\e[31mInstall oh-my-zsh? (Make sure to install zsh first)\e[0m" -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]] then
+    sh omz_install.sh
+fi 
--- omz_install.sh
+++ omz_install.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 ############################
 # This script creates symlinks from the home directory to any desired dotfiles in ~/.dotfiles
 ############################
@@ -12,36 +12,33 @@ ohmyzshfiles="mytheme.zsh-theme plugins zshrc" #oh-my-zsh files that are located
 # Test to see if zshell is installed.  If it is:
 if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
     
-    # Clone oh-my-zsh into .oh-my-zsh using wget
+    # Clone oh-my-zsh into .oh-my-zsh
     if [ ! -d ~/.oh-my-zsh/ ]; then
-        echo -e "[31mInstalling oh-my-zsh"
-        wget --no-check-certificate http://install.ohmyz.sh -O - | sh    
+        echo -e "\e[46mInstalling oh-my-zsh\e[0m"
+        git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
     fi
     
     # symlink oh-my-zsh theme and plugins and zshrc
-    echo -e "[31mCreating symlink to mytheme.zsh-theme in oh-my-zsh/themes/"
-    rm ~/.oh-my-zsh/themes/mytheme.zsh-theme
+    echo -e "\e[46mCreating symlink to mytheme.zsh-theme in oh-my-zsh/themes/\e[0m"
+    rm -f ~/.oh-my-zsh/themes/mytheme.zsh-theme
     ln -s $dir/mytheme.zsh-theme ~/.oh-my-zsh/themes/mytheme.zsh-theme
     
-    echo -e "[31mCreating symlink to custom plugins in oh-my-zsh/custom/plugins/"
-    rm ~/.oh-my-zsh/custom/plugins/ -r
+    echo -e "\e[46mCreating symlink to custom plugins in oh-my-zsh/custom/plugins/\e[0m"
+    rm -rf ~/.oh-my-zsh/custom/plugins
     ln -s $dir/plugins ~/.oh-my-zsh/custom/
     
-    echo -e "[31mInstalling zsh-syntax-highlighting"
-    cd $dir/plugins
-    git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
-    cd $dir
+    echo -e "\e[46mInstalling zsh-syntax-highlighting\e[0m"
+    git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $dir/plugins/zsh-syntax-highlighting
     
-    echo -e "[31mSetting up zshrc"
-    mv ~/.$file $olddir
+    echo -e "\e[46mSetting up zshrc\e[0m"
+    mv ~/.zshrc $olddir
     ln -s $dir/zshrc ~/.zshrc
 
     # Set the default shell to zsh if it isn't currently set to zsh
-    echo -e "[31mDon't forget to change zsh to default shell! (chsh -s zsh)"
+    echo -e "\e[31mDon't forget to change zsh to default shell! (chsh -s zsh)\e[0m"
     #chsh -s $(which zsh)
 
-
 else
-   echo -e "[31mPlease install zsh, then re-run this script!"
+   echo -e "\e[31mPlease install zsh, then re-run this script!\e[0m"
    exit
 fi
