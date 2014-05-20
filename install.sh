73133411d82b6f77121c37751a3cf0f97fd329f7
Matt Molo <molo.matt@gmail.com>
2014-05-20 16:47:18 -0500

    even better install + arch detecting aliases

--- install.sh
+++ install.sh
@@ -10,11 +10,13 @@ files="bashrc aliases vimrc vim"          # list of files/folders to symlink in 
 ##########
 
 # create dotfiles_old in homedir
-echo -e "\e[46mCreating $olddir for backup of any existing dotfiles in ~\e[0m"
+echo -e "\e[36mCreating $olddir for backup of any existing dotfiles in ~\e[0m"
 
 if [ ! -d $olddir/ ]; then
-    read -p "\e[31m$olddir already exists. Delete it?\e[0m" -n 1 -r
-    if [[ $REPLY =~ ^[Yy]$ ]] then
+    echo -e "\e[31m$olddir already exists. Delete it?\e[0m \c"
+    read -p "" -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
         echo -e "\e[31mRemoving $olddir\e[0m"
         rm -r $olddir
     fi    
@@ -24,15 +26,17 @@ mkdir -p $olddir
 
 # move any existing dotfiles in homedir to old dotfiles directory, then create symlinks from the homedir to any files in the dotfiles directory specified in $files
 cd $dir
-echo -e "\e[46mMoving any existing dotfiles from ~ to $olddir\e[0m"
+echo -e "\e[36mMoving any existing dotfiles from ~ to $olddir\e[0m"
 for file in $files; do
-    mv ~/.$file $olddir
-    echo -e "\e[46mCreating symlink to $file in home directory\e[0m"
+    mv ~/.$file $olddir 2>/dev/null
+    echo -e "\e[36mCreating symlink to $file in home directory\e[0m"
     ln -s $dir/$file ~/.$file
 done
 
 # ask to install oh-my-zsh
-read -p "\e[31mInstall oh-my-zsh? (Make sure to install zsh first)\e[0m" -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]] then
+echo -e "\e[31mInstall oh-my-zsh? (Make sure to install zsh first)\e[0m \c"
+read -p "" -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]] 
+then
     sh omz_install.sh
 fi 
--- omz_install.sh
+++ omz_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ############################
-# This script creates symlinks from the home directory to any desired dotfiles in ~/.dotfiles
+# This script installs oh-my-zsh and sets it up
+# Only run after install.sh has been run first!
 ############################
 
 ########## Variables
@@ -9,34 +10,41 @@ olddir=~/dotfiles_old             # old dotfiles backup directory
 ohmyzshfiles="mytheme.zsh-theme plugins zshrc" #oh-my-zsh files that are located in diff places
 ##########
 
-# Test to see if zshell is installed.  If it is:
+# Test to see if zsh is installed.  If it is:
 if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
     
     # Clone oh-my-zsh into .oh-my-zsh
     if [ ! -d ~/.oh-my-zsh/ ]; then
-        echo -e "\e[46mInstalling oh-my-zsh\e[0m"
+        echo -e "\e[36mInstalling oh-my-zsh\e[0m"
         git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
     fi
     
     # symlink oh-my-zsh theme and plugins and zshrc
-    echo -e "\e[46mCreating symlink to mytheme.zsh-theme in oh-my-zsh/themes/\e[0m"
+    echo -e "\e[36mCreating symlink to mytheme.zsh-theme in oh-my-zsh/themes/\e[0m"
     rm -f ~/.oh-my-zsh/themes/mytheme.zsh-theme
     ln -s $dir/mytheme.zsh-theme ~/.oh-my-zsh/themes/mytheme.zsh-theme
     
-    echo -e "\e[46mCreating symlink to custom plugins in oh-my-zsh/custom/plugins/\e[0m"
+    echo -e "\e[36mCreating symlink to custom plugins in oh-my-zsh/custom/plugins/\e[0m"
     rm -rf ~/.oh-my-zsh/custom/plugins
     ln -s $dir/plugins ~/.oh-my-zsh/custom/
     
-    echo -e "\e[46mInstalling zsh-syntax-highlighting\e[0m"
+    echo -e "\e[36mInstalling zsh-syntax-highlighting\e[0m"
     git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $dir/plugins/zsh-syntax-highlighting
     
-    echo -e "\e[46mSetting up zshrc\e[0m"
-    mv ~/.zshrc $olddir
+    echo -e "\e[36mSetting up zshrc\e[0m"
+    mkdir -p $olddir #just in case it was deleted
+    mv ~/.zshrc $olddir 2>/dev/null
     ln -s $dir/zshrc ~/.zshrc
 
     # Set the default shell to zsh if it isn't currently set to zsh
-    echo -e "\e[31mDon't forget to change zsh to default shell! (chsh -s zsh)\e[0m"
-    #chsh -s $(which zsh)
+    echo -e "\e[31mChange default shell to zsh?\e[0m \c"
+    read -p "" -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        chsh -s $(which zsh)
+    fi    
+
+    exit
 
 else
    echo -e "\e[31mPlease install zsh, then re-run this script!\e[0m"
